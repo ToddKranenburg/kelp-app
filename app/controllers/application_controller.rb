24c2bb3a1fb36964bd764c114615406de48dede4
Todd Kranenburg <todd.kranenburg@gmail.com>
2016-01-26 14:19:37 -0500

    Complete sign up feature and styling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def current_user
+   @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def login!(user)
+   user.reset_session_token!
+   session[:session_token] = user.session_token
+   @current_user = user
+ end
+
+  def logout!(user)
+    user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_sign_in
+    unless logged_in?
+      flash[:errors] = ["You must sign in first!"]
+      redirect_to new_user_url
+    end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+  validates :username, uniqueness: true, presence: true
+  validates :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(username, password)
+    possible_user = User.find_by_username(username)
+
+    return nil unless possible_user
+
+    if possible_user.has_password?(password)
+      possible_user
+    else
+      nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def has_password?(password)
+    actual_password = BCrypt::Password.new(self.password_digest)
+    actual_password.is_password?(password)
+  end
+
+end
