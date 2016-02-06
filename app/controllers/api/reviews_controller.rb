6f0b3425a7522f476ba9d2d98e16d91f76ade471
Todd Kranenburg <toddkranenburg@Todds-MacBook-Air.local>
2016-02-06 17:29:54 -0500

    Fix one more school bug.

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -8,7 +8,7 @@ class Api::ReviewsController < ApplicationController
       @reviews = Review.includes(:author).where("author_id = ?", user_id).includes(:business).order(created_at: :desc)
     elsif (owner_id)
       owner = User.find(owner_id)
-      @school_members = owner.school_members.includes(:review)
+      @school_members = owner.school_members.includes(:reviews)
       render "api/reviews/school_reviews_index"
     else
       @reviews = Review.includes(:author).includes(:business).order(created_at: :desc).all
